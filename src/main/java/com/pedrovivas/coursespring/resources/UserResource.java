aa9e430c1afd4cd546493de30f0b852e3dc381bb
Pedrobvivas <pedrobvivas@hotmail.com>
2023-10-14 18:34:55 -0300

    User and resource

---
+++ src/main/java/com/pedrovivas/coursespring/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.pedrovivas.coursespring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedrovivas.coursespring.entites.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1,"Maria","Maria@gmail.com","9999999","123");
+		return ResponseEntity.ok().body(u);
+	}
+}
