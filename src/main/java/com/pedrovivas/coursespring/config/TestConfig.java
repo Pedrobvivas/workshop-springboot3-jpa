fe6c40070c42ae78bed5c71fe32b24493ccfd39c
Pedrobvivas <pedrobvivas@hotmail.com>
2023-10-18 13:47:18 -0300

    New entity and methods

--- src/main/java/com/pedrovivas/coursespring/config/TestConfig.java
+++ src/main/java/com/pedrovivas/coursespring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pedrovivas.coursespring.entites.Category;
 import com.pedrovivas.coursespring.entites.Order;
 import com.pedrovivas.coursespring.entites.OrderItem;
+import com.pedrovivas.coursespring.entites.Payment;
 import com.pedrovivas.coursespring.entites.Product;
 import com.pedrovivas.coursespring.entites.User;
 import com.pedrovivas.coursespring.entites.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepositoy.save(o1);
 	}
 }
